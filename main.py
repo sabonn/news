73995400d21b208a91eff0db28b20291780817cf
sabonn <itay.eshkar@gmail.com>
2023-02-17 16:56:23 +0200

    being able to get titles of articles that are related to a subject and the links for the articles

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from news import execute
+import news
 
-execute("israel")
+search = 'israel'#searching for news
+url = news.text_url(search)
+html = news.get_html('h3',url)
+urls, titles = news.getting_titles_urls(html)
--- news.py
+++ news.py
@@ -1,40 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def get_html(search):
-    url = text_url(search)
+#start of scrap functios
+def get_html(search, url):#getting the html from the url that was passed
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    titles = soup.find_all('h3')
+    titles = soup.find_all(search)
     return titles
 
 
-def text_url(search):
+def text_url(search):#taking a search topic and turning it into a news.google url to search for articles
     search = search.replace(' ', '+')
     url = f'https://news.google.com/search?q={search}'
     return url
 
 
-def title_url(titles):
-    urls = []
-    for title in titles:
-        link = title.find('a')
-        if link:
-            url = link['href']
-            url = f'https://news.google.com{url[1:]}'
-            if url in urls:
-                continue
-            urls.append(url)
-    return urls
-
-def get_title(urls):
-    link = urls[0]
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    titles = soup.find_all('h1')
-    return titles[0]
-
-def execute(inp):
-    titles = get_html(inp)
-    urls = title_url(titles)
+def getting_titles_urls(html):#getting the titles of the articles and links for them
+    urls = set()
+    texts = []
+    for i in html:
+        link = i.find('a')
+        if link is not None:
+            url = f'https://news.google.com{link["href"][1:]}'
+            if url not in urls:
+                urls.add(url)
+        text = link.text.strip() if link is not None else ''
+        if text and text not in texts:
+            texts.append(text)
+    return (list(urls), texts)
+#end of scrap functions
