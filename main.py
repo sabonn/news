4a92b4b87b2e5e2b1b3af031a4a626d27ff2346f
sabonn <itay.eshkar@gmail.com>
2023-02-23 23:14:58 +0200

    everything is working great

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ search = 'israel'#searching for news
 url = news.text_url(search)
 html = news.get_html('h3',url)
 urls, titles = news.getting_titles_urls(html)
+for i in range(len(titles)):
+    print(f'title: {titles[i]}\nurl: {urls[i]}')
